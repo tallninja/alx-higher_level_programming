0a43ad97c2cb2756dcdc6a1214c13b2968bf459d
Ernest Wambua <ernestwambua2@gmail.com>
2022-05-31 11:41:49 +0300

    Fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(100):
     if i == 99:
         print("{:d}".format(i))
     else:
-        print("{}{:d}".format("0" if i < 10 else "", i), end=", ")
+        print("{:s}{:d}".format("0" if i < 10 else "", i), end=", ")
