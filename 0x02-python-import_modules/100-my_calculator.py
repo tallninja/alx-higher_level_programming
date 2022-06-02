b5e375180a4ef2d069b935f6299a0ba80da8a6a7
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-02 13:19:43 +0300

    100-my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     a = int(argv[1])
     op = argv[2]
     b = int(argv[3])
