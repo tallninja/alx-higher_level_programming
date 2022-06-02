da9e6073375e980ba00894b63221e10a02c9e030
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-02 09:03:40 +0300

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sys import argv
+
+count = len(argv) - 1
+argmnt = "argument"
+
+if count > 1:
+    argmnt += "s"
+
+if count == 0:
+    argmnt += "."
+else:
+    argmnt += ":"
+
+print("{:d} {:s}".format(count, argmnt))
+
+for i in range(count):
+    print("{:d}: {:s}".format((i + 1), argv[i + 1]))
