5785dc82c6f87f646f6ec9ae2d955ca83fbdeabe
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-02 09:33:33 +0300

    Import execution guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == '__main__':
+    from sys import argv
 
-count = len(argv) - 1
-argmnt = "argument"
+    count = len(argv) - 1
+    argmnt = "argument"
 
-if count > 1:
-    argmnt += "s"
+    if count > 1:
+        argmnt += "s"
 
-if count == 0:
-    argmnt += "."
-else:
-    argmnt += ":"
+    if count == 0:
+        argmnt += "."
+    else:
+        argmnt += ":"
 
-print("{:d} {:s}".format(count, argmnt))
+    print("{:d} {:s}".format(count, argmnt))
 
-for i in range(count):
-    print("{:d}: {:s}".format((i + 1), argv[i + 1]))
+    for i in range(count):
+        print("{:d}: {:s}".format((i + 1), argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == '__main__':
+    from sys import argv
 
-argc = len(argv) - 1
-result = 0
+    argc = len(argv) - 1
+    result = 0
 
-for i in range(argc):
-    result += int(argv[i +1])
+    for i in range(argc):
+        result += int(argv[i +1])
 
-print(result)
+    print(result)
