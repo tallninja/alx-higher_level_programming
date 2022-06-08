7f048da3145a0397dc2fe00a2092afd77c40da99
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-08 19:07:17 +0300

    1-search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if my_list and search and replace:
-        copy = [val for val in my_list]
-        for i in range(len(copy)):
-            if copy[i] == search:
-                copy[i] = replace
-        return (copy)
+    copy = [val for val in my_list]
+    for i in range(len(copy)):
+        if copy[i] == search:
+            copy[i] = replace
+    return (copy)
