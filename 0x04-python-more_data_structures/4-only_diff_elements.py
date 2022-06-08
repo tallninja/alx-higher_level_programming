0e83ee7c552e742c7df8b5d37d2ff4fa36607ae2
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-08 19:10:01 +0300

    Hot fix

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    if set_1 and set_2:
-        return (list(set_1 - set_2) + list(set_2 - set_1))
+    return (list(set_1 - set_2) + list(set_2 - set_1))
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
-    if a_dictionary:
-        return (len([keys for keys in a_dictionary.keys()]))
+    return (len([keys for keys in a_dictionary.keys()]))
