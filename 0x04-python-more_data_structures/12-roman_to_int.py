cbe7be239ca3ece78626f8f27ca6a7abcf8ad309
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-08 18:50:56 +0300

    12-roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,3 +6,4 @@ def roman_to_int(roman_string):
             'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
         }
         return (sum([roman_numbers.get(char) for char in roman_string]))
+    return (0)
