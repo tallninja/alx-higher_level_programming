ce2501dc2ad96c916bd965a61efe8d3b3ef2a189
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-20 14:55:03 +0300

    0-safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    elements = ""
+    try:
+        for i in range(x):
+            elements += str(my_list[i])
+            n += 1
+    except Exception as e:
+        pass
+    finally:
+        print("{}".format(elements))
+        return (n);
+    
+
+        
