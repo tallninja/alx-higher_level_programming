7f1fe36e92980059b805210bad66eb6d47bebc17
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-27 16:50:24 +0300

    9-rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,31 +1,45 @@
 #!/usr/bin/python3
-
-"""
-class Rectangle defines a rectangle
-"""
+"""Rectangle - defines a rectangle"""
 
 
-class Rectangle:
-    """defines a rectangle"""
+class Rectangle():
+    """Rectangle - defines a rectangle"""
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """constructor"""
-        
-        self.height = height
+        """The _init_ method defines the properties of Rectangle"""
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
+
+    def __str__(self):
+        """The _str_ method print a Rectangle with #"""
+        acum = ""
+        if (self.width * self.height > 0):
+            for x in range(self.height):
+                acum += (str(self.print_symbol) * self.width) + "\n"
+            acum = acum[:-1]
+        return acum
+
+    def __repr__(self):
+        """The _repr_ method return a string of Rectangle instance"""
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        """The _del_ method print a message when an instance is deleted"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
-        """width getter"""
+        """Return property width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """width setter"""
+        """Set property width"""
         if (type(value) is not int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -35,12 +49,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """height getter"""
+        """Return property height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """height setter"""
+        """Set property height"""
         if (type(value) is not int):
             raise TypeError("height must be an integer")
         elif value < 0:
@@ -49,43 +63,25 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-        """returns the rectangle area"""
-        return (self.__width * self.__height)
+        """Return area of Rectangle"""
+        return self.width * self.height
 
     def perimeter(self):
-        """returns the rectangle perimeter"""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return (2 * (self.__width + self.__height))
+        """Return perimeter of Rectangle"""
+        if (self.width == 0 or self.height == 0):
+            return 0
+        return 2 * (self.width + self.height)
 
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-        str_rep = ""
-        for i in range(self.__height):
-            str_rep += (str(self.print_symbol) * self.__width)
-            if i < self.__height - 1:
-                str_rep += "\n"
-        return (str_rep)
-
-    def __repr__(self):
-        return (f"Rectangle({self.__width}, {self.__height})")
-
-    def __del__(self):
-        """destructor"""
-        print("Bye rectangle...")
-        type(self).number_of_instances -= 1
-
-    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """returns the biggest rectangle based on the area"""
-        if type(rect_1) is not Rectangle:
+        """bigger_or_equal - return bigger rectangle"""
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) is not Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
-            return (rect_1)
-        return (rect_2)
+            return rect_1
+        else:
+            return rect_2
 
     @classmethod
     def square(cls, size=0):
