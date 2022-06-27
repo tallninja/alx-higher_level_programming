6d3744dac8194a26e8975b1d666be4a3de06604a
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-27 14:52:36 +0300

    2-rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle defines a rectangle
 class Rectangle:
     """defines a rectangle"""
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
