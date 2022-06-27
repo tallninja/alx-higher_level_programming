5fd8564be6757f1e4a714d66c3a3e4a4d9426f44
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-27 15:05:36 +0300

    3-rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -8,15 +8,17 @@ class Rectangle defines a rectangle
 class Rectangle:
     """defines a rectangle"""
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
+        """width getter"""
         return (self.__width)
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -25,10 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """height getter"""
         return (self.__height)
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -41,6 +45,8 @@ class Rectangle:
 
     def perimeter(self):
         """returns the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return (2 * (self.__width + self.__height))
 
     def __str__(self):
