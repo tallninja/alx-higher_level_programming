24da9ea79798cecc839d9d412c0f4d7f94c704e7
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-27 16:46:35 +0300

    9-rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -5,7 +5,7 @@ class Rectangle defines a rectangle
 """
 
 
-class Rectangle():
+class Rectangle:
     """defines a rectangle"""
 
     number_of_instances = 0
@@ -13,39 +13,37 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """constructor"""
-        self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        self.width = width
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
         """width getter"""
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
         """width setter"""
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
         """height getter"""
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
         """height setter"""
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
         """returns the rectangle area"""
