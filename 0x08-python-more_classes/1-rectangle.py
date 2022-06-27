65d4a9784e2090b923e646dfbd625b10fd4c00e4
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-27 14:45:56 +0300

    1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,10 +13,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """width getter"""
         return (self.__width)
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -25,10 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """height getter"""
         return (self.__height)
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
