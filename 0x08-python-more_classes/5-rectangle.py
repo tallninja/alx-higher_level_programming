845009d66172a6d1fae84b8be0b35fe9928e3470
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-27 15:17:02 +0300

    5-rectangle

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -8,15 +8,18 @@ class Rectangle defines a rectangle
 class Rectangle:
     """defines a rectangle"""
     def __init__(self, width=0, height=0):
+        """constructor"""
         self.__height = height
         self.__width = width
 
     @property
     def width(self):
+        """width getter"""
         return (self.__width)
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -25,10 +28,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """height getter"""
         return (self.__height)
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -41,9 +46,13 @@ class Rectangle:
 
     def perimeter(self):
         """returns the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return (2 * (self.__width + self.__height))
 
     def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ("")
         str_rep = ""
         for i in range(self.__height):
             str_rep += ("#" * self.__width)
@@ -55,4 +64,5 @@ class Rectangle:
         return (f"Rectangle({self.__width}, {self.__height})")
 
     def __del__(self):
+        """destructor"""
         print("Bye rectangle...")
