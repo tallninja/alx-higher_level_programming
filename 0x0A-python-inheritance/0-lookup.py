fc81cc38d2080f76bf5e39de9d46aae1417c0f7c
Ernest Wambua <ernestwambua2@gmail.com>
2022-07-04 16:26:00 +0300

    0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+returns a list of available attributes and methods of an object
+"""
+
 
 def lookup(obj):
     """returns a list of available attributes and methods of an object"""
