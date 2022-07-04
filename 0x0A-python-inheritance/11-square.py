bd50d95ac13f62b5ed748542d5243084ac4ea097
Ernest Wambua <ernestwambua2@gmail.com>
2022-07-04 17:01:36 +0300

    11-square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,9 +7,10 @@ class Square - implements a square from a rectangle
 
 Rectangle = __import__("9-rectangle").Rectangle
 
+
 class Square(Rectangle):
     """implements a square from a rectangle"""
-    
+
     def __init__(self, size):
         """constructor"""
         self.integer_validator("size", size)
