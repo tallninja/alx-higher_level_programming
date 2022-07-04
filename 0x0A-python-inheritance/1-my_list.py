aa3c1563407c07452c0eb0a5444b7e0b2c494b03
Ernest Wambua <ernestwambua2@gmail.com>
2022-07-04 16:41:51 +0300

    1-my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,6 +8,7 @@ print_sorted - that prints the list, but sorted (ascending sort)
 
 class MyList(list):
     """My List"""
+
     def print_sorted(self):
         """prints the list in ascending order"""
         copy = self[:]
