57cd4440b5be70a7e6f9755018a8ee6d86441807
Ernest Wambua <ernestwambua2@gmail.com>
2022-07-07 18:11:16 +0300

    display handle x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,8 +82,10 @@ class Rectangle(Base):
 
     def display(self):
         """prints to stdout the layout of rectangle"""
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print((" " * self.__x) + ("#" * self.__width))
 
     def __str__(self):
         """string representation of rectangle object"""
