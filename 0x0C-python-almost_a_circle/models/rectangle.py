4457042295f79bf224a852126d1533095e857718
Ernest Wambua <ernestwambua2@gmail.com>
2022-07-07 18:23:44 +0300

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,3 +95,16 @@ class Rectangle(Base):
         width = self.__width
         height = self.__height
         return f"[Rectangle] ({id}) {x}/{y} - {width}/{height}"
+
+    def update(self, *args):
+        """updates the class"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
